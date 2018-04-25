d2957fb7741f8c904994a8328adff4fc9b261fa1
maximlevinov <maximlevinov@users.noreply.github.com>
2018-04-25 09:16:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-const mongoose = require('mongoose');
 const path = require('path');
-
 
 //git check
 var app = express()
@@ -11,48 +9,52 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 })); 
 // app.use(express.static(path.join(__dirname, 'web-client/build')));
-
+//db connection
+const mongoose = require('mongoose');
 mongoose.connect('');
-
 var Schema = mongoose.Schema;
-
-var FeedSchema = new Schema(
-    {
-        name: {type: String, required: true, max: 99},
-        sensor: {type: String, required: true, max: 99},
-        data : {type: Number, required: true},
-        time : {type: Number, required: true},
-    }
-);
-
+var FeedSchema = new Schema({
+	name: {type: String, required: true, max: 99},
+	sensor: {type: String, required: true, max: 99},
+	data : {type: Number, required: true},
+	time : {type: Number, required: true},
+});
 var db = mongoose.connection;
-
 var MyModel = db.model('Feed', FeedSchema);
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//main app page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/public/index.html');
+});
+app.get('*', (req, res) => {
+    res.sendFile(__dirname+'/public/'+req.path);
+});
 
+//device data listener
+var appData = {"reqParams":["name","sensor","data","time"],"port":8000};
 app.post('/feed', function (req, res) {
-
-    var instance = new MyModel();
-    instance.name = req.body.name;
-    instance.sensor = req.body.sensor;
-    instance.data = req.body.data;
-    instance.time = req.body.time;
-    
+    var instance = new MyModel();	
+	appData.reqParams.map( o => {
+		instance[o]=req.body[o];
+	});
     instance.save(function (err, feed) {
-        res.json(feed._id)        
+		if (err) {
+			res.send(err._message);
+		} else {
+			res.json(feed._id)
+		}
     });
-    // res.send('Hello World!')
 })
-
+//query by Index
 app.post('/feedsByIndex', function(req,res){
     var query = {};
-    query[req.body.index] = req.body.value;
+    query[req.body.index] = req.body.value;	
     MyModel.find(query).then(function(x){
+		console.log(x)
         res.json(x)
-    })
+    });
 })
-
+//query by timeframe
 app.post('/feedsByTime', function(req,res){
     var query = {
         time : { 
@@ -65,6 +67,24 @@ app.post('/feedsByTime', function(req,res){
     })
 })
 
+app.post('/general', function(req,res){
+	if(req.body.index === 'name'){
+		MyModel.distinct("name").then(function(data){
+			res.send(data)
+		});
+	} else if (req.body.index === 'sensor' && req.body.value === undefined){
+		MyModel.distinct(req.body.index).then(function(data){
+			res.send(data)
+		});
+	} else if (req.body.index === 'sensor' && req.body.value !== undefined){
+		MyModel.distinct(req.body.index,{"name":req.body.value}).then(function(data){
+			res.send(data)
+		});
+	}
+	else {
+		res.status(400);
+	}
+})
 // app.get('/', function(req,res){
 //     res.send("wowooooooooo")
 // })
@@ -93,10 +113,7 @@ app.post('/feedsByTime', function(req,res){
 //     MyModel.findById(req.body.id).then(x => res.json(x));
 // })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
 
-app.listen(process.env.PORT || 8000, function () {
-  console.log('Example app listening on port 8000!')
+var server = app.listen(process.env.PORT ? process.env.PORT : appData.port, function () {
+	console.log('Example app listening on port '+ (process.env.PORT ? process.env.PORT : appData.port));
 })
