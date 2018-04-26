9174cd42715c1cc2e7f742098b165fc31e630928
Ido Avidan <IDOA@NIPZWE.ZW>
2018-04-26 11:15:35 +0200

    add group to model

--- models/feedModel.js
+++ models/feedModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var FeedSchema = new Schema({
+    group : {type: String, required : false, max : 99},
 	name: {type: String, required: true, max: 99},
 	sensor: {type: String, required: true, max: 99},
 	data : {type: Number, required: true},
