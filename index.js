e22d108deeeefb4570a833580f047cc205b6c189
Ido Avidan <IDOA@NIPZWE.ZW>
2018-05-02 09:19:03 +0200

    spaces

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 const path = require('path');
-
 const mongoose = require('mongoose');
-
-
 var config = require('./private/config.json');
 
 //git check
