4b31c4faf4d195025a66afc0a50dc59bd2851e09
Ido Avidan <IDOA@NIPZWE.ZW>
2018-05-09 11:33:44 +0200

    queryAPI now return status code

--- dbs/mongoDB.js
+++ dbs/mongoDB.js
@@ -1,8 +1,8 @@
 module.exports.save = function(instance, callback){
     instance.save(callback);
 }
-module.exports.find = function(instance, query, callback){
-    instance.find(query).then(callback);
+module.exports.find = function(instance, query, exclude , callback){
+    instance.find(query, exclude).then(callback);
 }
 module.exports.findOne = function(instance,query, exclude, callback){
     instance.findOne(query,exclude).then(callback)
--- routes/queryAPI.js
+++ routes/queryAPI.js
@@ -6,6 +6,14 @@ var appData = config.appData;
 var FeedModel = require('../models/feedModel');
 var db = require('../dbs/mongoDB');
 
+function errhandle(error, data,res){
+	console.log(data)
+	if((error)||(data === null)||(data.length === 0)){
+		res.status(500).send(error)
+	}
+	else res.json(data)
+}
+
 router.post('/feedsByIndex', function(req,res){
     var query = {};
 	query[req.body.index] = req.body.value;	
@@ -15,45 +23,27 @@ router.post('/feedsByIndex', function(req,res){
             $lt : req.body.to 
 		}
 	}
-    db.find(FeedModel,query,function(x){
-        res.json(x)
-    })
+    db.find(FeedModel,query, (err,data) => errhandle(err,data,res))
 })
+
 
 var GroupModel = require('../models/groupModel');
 
-function getGroup(group_id,callback){
-	db.findOne(GroupModel,{group_id : group_id},
-		{_id : 0, __v : 0, "group_devices._id" : 0, "group_devices.device_sensors._id" : 0},
-	callback)		
-}
-
 router.post('/groups', function(req,res){
-	db.find(GroupModel,{}, x => res.json(x.map(group => group.group_id)));
+	db.find(GroupModel,query = {}, {group_id : 1, _id : 0}, (data,err) => errhandle(err,data ,res));
 })
-
-router.post('/devices', function(req,res){
-	getGroup(req.body.group_id,x => res.json(x.group_devices))
-
-})
-
+	
 router.post('/group', function(req,res){
-	getGroup(req.body.group_id, (x) => res.json(x))
+	query = {group_id : req.body.group_id};
+	exclude = {_id : 0, __v : 0, "group_devices._id" : 0, "group_devices.device_sensors._id" : 0};
+	db.findOne(GroupModel,query,exclude,(data,err) => errhandle(err,data,res))		
 })
-
-
-router.post('/group1', function(req,res){
+	
+router.post('/createGroup', function(req,res){
 	var instance = new GroupModel();	
 	instance.group_id = req.body.group_id;
 	instance.group_devices = req.body.group_devices;
-	db.save(instance,function (err, feed) {
-		if (err) {
-			res.send(err._message);
-		} else {
-			res.json(feed._id)
-		}
-	});
+	db.save(instance, (err,data) => errhandle(err,data,res))	
 })
-
 
 module.exports = router;
