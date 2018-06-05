923220d00d6c12b2a5c9b04ec376d92f9dc1680c
max <maximlevinov@users.noreply.github.com>
2018-06-05 16:23:16 +0200

    adde console bind for query api and updated no data response

--- routes/queryAPI.js
+++ routes/queryAPI.js
@@ -1,3 +1,4 @@
+var c = console.log.bind(console);
 var express = require('express');
 var router = express.Router();
 var config = require('../private/config.json');
@@ -7,80 +8,73 @@ var FeedModel = require('../models/feedModel');
 var db = require('../dbs/mongoDB');
 
 function errhandle(error, data,res){
-	if((error)||(data === null)||(data.length === 0)){
-		console.log(error)
-		res.status(500).send(error)
-	}
-	if((data === null)||(data.length === 0)){
-		console.log(error)
-		res.send("")
-	}
-	else res.json(data)
+    if(error){
+	c(error);
+	res.status(500).send(error);
+    } else if(data === null || data.length === 0){
+	c(error);
+	res.send("");
+    } else res.json(data);
 }
 
 router.post('/feedsByIndex', function(req,res){
     var query = {};
 	query[req.body.index] = req.body.value;	
 	if(req.body.from !== req.body.to){
-		query.time = {
-			$gt : req.body.from, 
-            $lt : req.body.to 
-		}
-	}
-    db.find(FeedModel,query, (err,data) => errhandle(err,data,res))
-})
-
+	    query.time = {
+		$gt : req.body.from, 
+		$lt : req.body.to 
+	    };
+	};
+    db.find(FeedModel,query, (err,data) => errhandle(err,data,res));
+});
 
 var GroupModel = require('../models/groupModel');
 
 router.post('/groups', function(req,res){
-	db.find(GroupModel,query = {}, {group_id : 1, _id : 0}, (data,err) => errhandle(err,data ,res));
-})
+    db.find(GroupModel,query = {}, {group_id : 1, _id : 0}, (data,err) => errhandle(err,data ,res));
+});
 	
 router.post('/group', function(req,res){
-	query = {group_id : req.body.group_id};
-	// query = req.body.group_id;
-	
-	// console.log(query)
-	exclude = {_id : 0, __v : 0, "group_devices._id" : 0, "group_devices.device_sensors._id" : 0};
-	db.findOne(GroupModel,query,exclude,(data,err) => errhandle(err,data,res))		
+    query = {group_id : req.body.group_id};
+    exclude = {_id : 0, __v : 0, "group_devices._id" : 0, "group_devices.device_sensors._id" : 0};
+    db.findOne(GroupModel,query,exclude,(data,err) => errhandle(err,data,res));		
 })
 	
 router.post('/createGroup', function(req,res){
-	var instance = new GroupModel();	
-	instance.group_id = req.body.group_id;
-	instance.group_devices = req.body.group_devices;
-	db.save(instance, (err,data) => errhandle(err,data,res))	
+    var instance = new GroupModel();	
+    instance.group_id = req.body.group_id;
+    instance.group_devices = req.body.group_devices;
+    db.save(instance, (err,data) => errhandle(err,data,res));	
 })
 
 //users
 var UserModel = require('../models/userModel');
 
 router.post('/registerUser', function(req,res){
-	var instance = new UserModel();	
-	instance.username = req.body.username;
-	instance.password = req.body.password;
-	instance.email = req.body.email;
-	instance.graphs = {g1 : "wow"};
-	console.log(instance)
-	db.save(instance, (err,data) => errhandle(err,data,res))	
+    var instance = new UserModel();	
+    instance.username = req.body.username;
+    instance.password = req.body.password;
+    instance.email = req.body.email;
+    instance.graphs = {g1 : "wow"};
+    c(instance);
+    db.save(instance, (err,data) => errhandle(err,data,res))	
 })
 
 router.post('/saveGraph', function(req,res){
-	exclude = {};
-	query = {username : req.body.username};
-	db.update(UserModel,query,exclude, function(err,user){
-		if (err) {res.json(err)}
-		user.set({graphs : req.body.graph})
-		db.save(user , (err,data) => errhandle(err,data,res));
-	})	
-})
+    exclude = {};
+    query = {username : req.body.username};
+    db.update(UserModel,query,exclude, function(err,user){
+	if (err) {res.json(err);};
+	user.set({graphs : req.body.graph});
+	db.save(user , (err,data) => errhandle(err,data,res));
+    });
+});
 
 router.post('/getGraph', function(req,res){
-	exclude = {};
-	query = {username : req.body.username};
-	db.findOne(UserModel,query,exclude, (data,err) =>  errhandle(err,data.graphs,res));
-		
-})
+    exclude = {};
+    query = {username : req.body.username};
+    db.findOne(UserModel,query,exclude, (data,err) =>  errhandle(err,data.graphs,res));
+});
 
 module.exports = router;
